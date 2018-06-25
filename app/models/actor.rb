cd3f5b3f52fe73d6c55a4fe72a71f5dbc78ea728
Ms-Alex <ms.hernandeza1@gmail.com>
2018-06-24 20:40:48 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    str = ""
+    self.characters.map do |char|
+      str += char.name + " - "
+      self.shows.map {|show| str += show.name}
+    end
+    str
+  end
+
 end
--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'spec_helper'
 
+
 describe Network do
   let(:network) { Network.new }
 
